1f4cbfa6ab78c5ce786acf897e6a947397812aef
Sunyoung yu <ysy1990117@gmail.com>
2012-04-28 16:38:22 +0900

    refactor

--- project-a/src/Gugudan.java
+++ project-a/src/Gugudan.java
@@ -8,25 +8,18 @@ public class Gugudan {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
 		System.out.println("gugudanfrom:");
 		Scanner sc = new Scanner(System.in);
-
 		int i=sc.nextInt();
-
 		printDan(i);
 
 	}
 
 	public static void printDan(int i) {
-		print(i);
-	}
-
-	public static void print(int i) {
 		for (int j = 1; j < 9; j++) {
 			int k= i * j;
 			System.out.println(i + " x" + j + " =" + k);
 		}
 	}
+
 }
